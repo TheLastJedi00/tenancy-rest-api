70ac57820f20d92ba9b3753ed9544004ed320a35
vyzent <williamdouglasgg@gmail.com>
2025-10-03 10:13:17 -0300

    feat: Entity priority

---
+++ src/main/java/ThinkDesk/Domain/Models/Status.java
@@ -0,0 +1,22 @@
+package ThinkDesk.Domain.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Status {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Tenant tenant;
+}
