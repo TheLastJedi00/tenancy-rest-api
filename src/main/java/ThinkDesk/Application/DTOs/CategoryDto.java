204389b940956d1c19807ea9154bc9910f15ec8d
TheLastJedi00 <jediaelborges23@gmail.com>
2025-10-08 15:26:54 -0300

    feat: category controller is created

--- src/main/java/ThinkDesk/Application/DTOs/CategoryDto.java
+++ src/main/java/ThinkDesk/Application/DTOs/CategoryDto.java
@@ -1,8 +1,14 @@
 package ThinkDesk.Application.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CategoryDto(
+        @NotBlank
         String name,
+        @NotBlank
         String description,
+        @NotNull
         Long tenantId
 ) {
 }
